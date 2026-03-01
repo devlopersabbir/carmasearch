cb3c02dbe18725e23183914012b2359015d7bf7b
devlopersabbir <devlopersabbir@gmail.com>
2026-03-01 10:26:39 +0600

    refactor: Simplify Elasticsearch search query construction, update project vision, and adjust default database port.

--- apps/server/internal/elastic/repository.go
+++ apps/server/internal/elastic/repository.go
@@ -17,211 +17,29 @@ func Search2(c context.Context, body *core.Vehicle) ([]uint64, int64, error) {
 }
 
 func Search(ctx context.Context, req *esCore.VehicleSearchQuery) ([]uint64, int64, error) {
-	log.Println("search request::::::::::::::::", &req)
 	if es.ESClient == nil {
 		return nil, 0, nil
 	}
 
 	mustArr := []interface{}{}
-	filterArr := []interface{}{}
-	shouldArr := []interface{}{}
-
-	// =========================
-	// Full Text Search
-	// =========================
-	if req.Query != "" {
-		mustArr = append(mustArr, map[string]interface{}{
-			"multi_match": map[string]interface{}{
-				"query":  req.Query,
-				"fields": []string{"title^3", "make^2", "model^2", "seller_name"},
-				"type":   "best_fields",
-			},
-		})
-	}
-
-	// =========================
-	// Exact Match Filters
-	// =========================
-	if req.VehicleID != nil {
-		filterArr = append(filterArr, map[string]interface{}{
-			"term": map[string]interface{}{
-				"vehicle_id.keyword": *req.VehicleID,
-			},
-		})
-	}
-
-	if req.Make != nil {
-		filterArr = append(filterArr, map[string]interface{}{
-			"term": map[string]interface{}{
-				"make.keyword": *req.Make,
-			},
-		})
-	}
-
-	if req.Model != nil {
-		filterArr = append(filterArr, map[string]interface{}{
-			"term": map[string]interface{}{
-				"model.keyword": *req.Model,
-			},
-		})
-	}
-
-	if req.SellerName != nil {
-		filterArr = append(filterArr, map[string]interface{}{
-			"term": map[string]interface{}{
-				"seller_name.keyword": *req.SellerName,
-			},
-		})
-	}
-
-	// =========================
-	// Multi Select Fields
-	// =========================
-	if len(req.FuelType) > 0 {
-		filterArr = append(filterArr, map[string]interface{}{
-			"terms": map[string]interface{}{
-				"fuel_type.keyword": req.FuelType,
-			},
-		})
-	}
-
-	if len(req.Transmission) > 0 {
-		filterArr = append(filterArr, map[string]interface{}{
-			"terms": map[string]interface{}{
-				"transmission.keyword": req.Transmission,
-			},
-		})
-	}
-
-	if len(req.ExteriorColors) > 0 {
-		filterArr = append(filterArr, map[string]interface{}{
-			"terms": map[string]interface{}{
-				"exterior_colors.keyword": req.ExteriorColors,
-			},
-		})
-	}
-
-	if len(req.InteriorColors) > 0 {
-		filterArr = append(filterArr, map[string]interface{}{
-			"terms": map[string]interface{}{
-				"interior_colors.keyword": req.InteriorColors,
-			},
-		})
-	}
-
-	if len(req.InteriorMaterials) > 0 {
-		filterArr = append(filterArr, map[string]interface{}{
-			"terms": map[string]interface{}{
-				"interior_materials.keyword": req.InteriorMaterials,
-			},
-		})
-	}
-
-	// =========================
-	// Range Filters
-	// =========================
-
-	addRangeFilter := func(field string, from interface{}, to interface{}) {
-		rangeQuery := map[string]interface{}{}
-
-		if from != nil {
-			rangeQuery["gte"] = from
-		}
-		if to != nil {
-			rangeQuery["lte"] = to
-		}
-
-		if len(rangeQuery) > 0 {
-			filterArr = append(filterArr, map[string]interface{}{
-				"range": map[string]interface{}{
-					field: rangeQuery,
-				},
-			})
-		}
-	}
-
-	addRangeFilter("price", req.PriceFrom, req.PriceTo)
-	addRangeFilter("registration_year", req.RegistrationFrom, req.RegistrationTo)
-	addRangeFilter("mileage", req.MileageFrom, req.MileageTo)
-	addRangeFilter("co2_emission", req.CO2EmissionFrom, req.CO2EmissionTo)
-
-	// =========================
-	// Boolean Feature Filters
-	// =========================
-	addBoolFilter := func(field string, value *bool) {
-		if value != nil && *value {
-			filterArr = append(filterArr, map[string]interface{}{
-				"term": map[string]interface{}{
-					field: true,
-				},
-			})
-		}
-	}
-
-	addBoolFilter("abs", req.ABS)
-	addBoolFilter("esp", req.ESP)
-	addBoolFilter("traction_control", req.TractionControl)
-	addBoolFilter("emergency_brake_assist", req.EmergencyBrakeAssist)
-	addBoolFilter("blind_spot_assist", req.BlindSpotAssist)
-	addBoolFilter("lane_assist", req.LaneAssist)
-	addBoolFilter("traffic_sign_recognition", req.TrafficSignRecognition)
-	addBoolFilter("isofix", req.ISOFIX)
-
-	addBoolFilter("heated_steering_wheel", req.HeatedSteeringWheel)
-	addBoolFilter("start_stop_system", req.StartStopSystem)
-	addBoolFilter("heated_seats", req.HeatedSeats)
-	addBoolFilter("electric_seats", req.ElectricSeats)
-	addBoolFilter("sport_seats", req.SportSeats)
-
-	addBoolFilter("fog_lights", req.FogLights)
-	addBoolFilter("adaptive_headlights", req.AdaptiveHeadlights)
-	addBoolFilter("rain_sensor", req.RainSensor)
-
-	addBoolFilter("radio", req.Radio)
-	addBoolFilter("navigation_system", req.NavigationSystem)
-	addBoolFilter("voice_control", req.VoiceControl)
-	addBoolFilter("bluetooth", req.Bluetooth)
-	addBoolFilter("usb", req.USB)
-	addBoolFilter("apple_carplay", req.AppleCarPlay)
-	addBoolFilter("android_auto", req.AndroidAuto)
-
-	// =========================
-	// Final Query
-	// =========================
+	// TODO: all searching, maching, comparing will be go here
 
 	var query interface{}
-
-	if len(mustArr) == 0 && len(filterArr) == 0 {
-		query = map[string]interface{}{
-			"match_all": map[string]interface{}{},
-		}
+	if len(mustArr) == 0 {
+		query = map[string]interface{}{"match_all": map[string]interface{}{}}
 	} else {
 		query = map[string]interface{}{
 			"bool": map[string]interface{}{
-				"must":   mustArr,
-				"filter": filterArr,
-				"should": shouldArr,
+				"must": mustArr,
 			},
 		}
 	}
-	log.Println("query::::::::::::::::", query)
-	sortArr := []interface{}{
-		map[string]interface{}{
-			req.SortBy: map[string]interface{}{
-				"order": req.SortOrder,
-			},
-		},
-	}
-
 	queryMap := map[string]interface{}{
 		"query": query,
 		"from":  (req.Page - 1) * req.PageSize,
 		"size":  req.PageSize,
-		"sort":  sortArr,
 	}
-
 	queryBytes, _ := json.Marshal(queryMap)
-
 	res, err := es.ESClient.Search(
 		es.ESClient.Search.WithContext(ctx),
 		es.ESClient.Search.WithIndex(es.ESIndexName),
@@ -264,15 +82,17 @@ func Search(ctx context.Context, req *esCore.VehicleSearchQuery) ([]uint64, int6
 	var ids []uint64
 	for _, hit := range hits {
 		if hitMap, ok := hit.(map[string]interface{}); ok {
-			if idStr, ok := hitMap["_id"].(string); ok {
-				idUint, _ := strconv.ParseUint(idStr, 10, 64)
-				ids = append(ids, idUint)
+			if source, ok := hitMap["_source"].(map[string]interface{}); ok {
+				if idFloat, ok := source["id"].(float64); ok {
+					ids = append(ids, uint64(idFloat))
+				}
 			}
 		}
 	}
 
 	return ids, total, nil
 }
+
 func IndexVehicle(s *core.Vehicle) {
 	jsonBody, err := json.Marshal(s)
 	if err != nil {
