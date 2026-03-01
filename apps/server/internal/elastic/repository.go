0c74a34a129f8d73c15250ae8e76d77e96d6ab6d
devlopersabbir <devlopersabbir@gmail.com>
2026-03-01 10:50:17 +0600

    feat: Implement dynamic Elasticsearch query construction and sorting, and update the project vision and core principles.

--- apps/server/internal/elastic/repository.go
+++ apps/server/internal/elastic/repository.go
@@ -5,7 +5,9 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/carmasearch/carma-server/api/vehicle/core"
 	es "github.com/carmasearch/carma-server/internal/database"
@@ -22,7 +24,75 @@ func Search(ctx context.Context, req *esCore.VehicleSearchQuery) ([]uint64, int6
 	}
 
 	mustArr := []interface{}{}
-	// TODO: all searching, maching, comparing will be go here
+
+	if req.Query != "" {
+		mustArr = append(mustArr, map[string]interface{}{
+			"query_string": map[string]interface{}{
+				"query": "*" + req.Query + "*",
+			},
+		})
+	}
+
+	v := reflect.ValueOf(req)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldValue := v.Field(i)
+		fieldType := t.Field(i)
+
+		jsonTag := strings.Split(fieldType.Tag.Get("json"), ",")[0]
+		if jsonTag == "" || jsonTag == "-" {
+			continue
+		}
+
+		if fieldValue.Kind() == reflect.Ptr && fieldValue.IsNil() {
+			continue
+		}
+
+		if fieldValue.Kind() == reflect.Slice && fieldValue.Len() == 0 {
+			continue
+		}
+
+		val := fieldValue.Interface()
+		if fieldValue.Kind() == reflect.Ptr {
+			val = fieldValue.Elem().Interface()
+		}
+
+		if strings.HasSuffix(jsonTag, "_from") {
+			field := strings.TrimSuffix(jsonTag, "_from")
+			mustArr = append(mustArr, map[string]interface{}{
+				"range": map[string]interface{}{
+					field: map[string]interface{}{
+						"gte": val,
+					},
+				},
+			})
+		} else if strings.HasSuffix(jsonTag, "_to") {
+			field := strings.TrimSuffix(jsonTag, "_to")
+			mustArr = append(mustArr, map[string]interface{}{
+				"range": map[string]interface{}{
+					field: map[string]interface{}{
+						"lte": val,
+					},
+				},
+			})
+		} else if fieldValue.Kind() == reflect.Slice {
+			mustArr = append(mustArr, map[string]interface{}{
+				"terms": map[string]interface{}{
+					jsonTag: val,
+				},
+			})
+		} else {
+			mustArr = append(mustArr, map[string]interface{}{
+				"match": map[string]interface{}{
+					jsonTag: val,
+				},
+			})
+		}
+	}
 
 	var query interface{}
 	if len(mustArr) == 0 {
@@ -38,6 +108,30 @@ func Search(ctx context.Context, req *esCore.VehicleSearchQuery) ([]uint64, int6
 		"query": query,
 		"from":  (req.Page - 1) * req.PageSize,
 		"size":  req.PageSize,
+	}
+
+	if req.SortBy != "" {
+		order := "desc"
+		if req.SortOrder != "" {
+			order = req.SortOrder
+		}
+
+		sortBy := req.SortBy
+		// Map `created_at` to `CreatedAt` because `core.Vehicle` misses the json tag for it.
+		if sortBy == "created_at" {
+			sortBy = "CreatedAt"
+		} else if sortBy == "updated_at" {
+			sortBy = "UpdatedAt"
+		}
+
+		queryMap["sort"] = []map[string]interface{}{
+			{
+				sortBy: map[string]interface{}{
+					"order": order,
+					"unmapped_type": "long",
+				},
+			},
+		}
 	}
 	queryBytes, _ := json.Marshal(queryMap)
 	res, err := es.ESClient.Search(
